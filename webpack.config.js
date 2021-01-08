01fcf11f1f924c3aca19d7705361f4eec6144425
BuildTools <unconfigured@null.spigotmc.org>
2021-01-08 13:19:08 +0900

    deployed using firebase hosting

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 "use strict";
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -18,7 +19,16 @@ module.exports = {
       }
     ]
   },
-  plugins: [],
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          context: "./public",
+          from: "*.*"
+        }
+      ]
+    })
+  ],
   devServer: {
     contentBase: "./public",
     host: "localhost",
